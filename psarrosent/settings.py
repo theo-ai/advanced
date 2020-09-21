ab6947c8376666d8be13d4b6fbb6dc5028afd77c
unknown <trelos_7@windowslive.com>
2020-09-21 16:55:26 -0700

    Inital Commit

--- psarrosent/settings.py
+++ psarrosent/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,3 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+django_heroku.settings(locals())
