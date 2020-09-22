94c0db50dd0a4e5beda5f6b4d6baf0c82a845b56
theo-ai <71651589+theo-ai@users.noreply.github.com>
2020-09-23 01:52:10 +0300

    Add files via upload

---
+++ test_ent/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('home',views.home, name='home'),
+    path('db',views.db, name='db'),
+    path('add',views.add, name='add'),
+    path('search_complete',views.search_complete, name='search_complete'),
+    path('insert_complete',views.insert_complete, name='insert_complete'),
+    path('update_complete',views.update_complete, name='update_complete'),
+    path('search_complete_client',views.search_complete_client, name='search_complete_client'),
+    path('insert_complete_client',views.insert_complete_client, name='insert_complete_client'),
+    path('update_complete_client',views.update_complete_client, name='update_complete_client'),
+    path('about',views.about, name='about'),
+    path('products',views.products, name='products'),
+    path('contact',views.contact, name='contact'),
+]
