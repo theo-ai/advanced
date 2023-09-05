1a8473b474fe62fa4b95cb8bc1203a72c2ee0f3d
theo-ai <71651589+theo-ai@users.noreply.github.com>
2023-09-06 01:44:36 +0300

    Update admin.py (Calendar)
    Calendar with capital C both line 2 and 6

--- test_ent/admin.py
+++ test_ent/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import calendar
+from .models import Calendar
 
 # Register your models here.
 
-admin.site.register(calendar)
+admin.site.register(Calendar)
