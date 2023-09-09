3206d80e1b0c79bd946366d856a4ac63a48f68a8
theo-ai <71651589+theo-ai@users.noreply.github.com>
2023-09-09 07:43:33 +0300

    Update views.py (added missing functions)

--- test_ent/views.py
+++ test_ent/views.py
@@ -77,6 +77,35 @@ def search_client(request):
 
     return render(request, 'search_client.html', {'clients': clients, 'query': query})
 
+def insert_client(request):
+    # Handle insert client logic here
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('db')
+    else:
+        form = ClientForm()
+  
+    return render(request, 'insert_client.html', {'form': form})
+
+def update_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('db')
+    else:
+        form = ClientForm(instance=client)
+
+    return render(request, 'update_client.html', {'form': form, 'client': client})
+
+def display_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    return render(request, 'display_client.html', {'client': client})
+
 def search_calendar(request):
     query = request.GET.get('q')
     results = Calendar.objects.filter(
@@ -109,39 +138,6 @@ def insert_calendar(request, client_id):
 
     return render(request, 'insert_calendar.html', {'form': form, 'client': client})
 
-def insert_client(request):
-    # Handle insert client logic here
-    if request.method == "POST":
-        form = ClientForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse("Client added successfully.")
-    else:
-        form = ClientForm()
-  
-    return render(request, 'insert_client.html', {'form': form})
-
-def display_client(request, client_id):
-    client = get_object_or_404(Client, pk=client_id)
-    return render(request, 'display_client.html', {'client': client})
-
-def update_client(request, client_id):
-    client = get_object_or_404(Client, pk=client_id)
-
-    if request.method == 'POST':
-        form = ClientForm(request.POST, instance=client)
-        if form.is_valid():
-            form.save()
-            return redirect('db')
-    else:
-        form = ClientForm(instance=client)
-
-    return render(request, 'update_client.html', {'form': form, 'client': client})
-
-def display_calendar(request, calendar_id):
-    calendar_entry = get_object_or_404(Calendar, pk=calendar_id)
-    return render(request, 'display_calendar.html', {'calendar_entry': calendar_entry})
-
 def update_calendar(request, calendar_id):
     calendar_entry = get_object_or_404(Calendar, pk=calendar_id)
 
@@ -154,6 +150,65 @@ def update_calendar(request, calendar_id):
         form = CalendarForm(instance=calendar_entry)
 
     return render(request, 'update_calendar.html', {'form': form, 'calendar_entry': calendar_entry})
+
+def display_calendar(request, calendar_id):
+    calendar_entry = get_object_or_404(Calendar, pk=calendar_id)
+    return render(request, 'display_calendar.html', {'calendar_entry': calendar_entry})
+
+# Search Complete view for both client and calendar
+def search_complete(request):
+    if request.method == 'POST':
+        search_params = request.POST.get('search_params')
+        # You can define your search criteria for both client and calendar here
+        clients = Client.objects.filter(
+            # Define your client search criteria based on search_params
+        )
+        calendars = Calendar.objects.filter(
+            # Define your calendar search criteria based on search_params
+        )
+        return render(request, 'search_complete.html', {'clients': clients, 'calendars': calendars})
+    return render(request, 'search_complete.html')
+
+# Insert Complete view for both client and calendar
+def insert_complete(request):
+    if request.method == 'POST':
+        # Implement the logic to insert data for both client and calendar here
+        pass
+
+# Update Client Complete view
+def update_client_complete(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('search_client')
+
+    return render(request, 'update_client_complete.html', {'form': form, 'client': client})
+
+# Insert Calendar Complete view
+def insert_calendar_complete(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    if request.method == 'POST':
+        form = CalendarForm(request.POST)
+        if form.is_valid():
+            calendar = form.save(commit=False)
+            calendar.client = client
+            calendar.save()
+            return redirect('search_calendar')
+
+    return render(request, 'insert_calendar_complete.html', {'form': form, 'client': client})
+
+# Update Calendar Complete view
+def update_calendar_complete(request, calendar_id):
+    calendar = get_object_or_404(Calendar, id=calendar_id)
+    if request.method == 'POST':
+        form = CalendarForm(request.POST, instance=calendar)
+        if form.is_valid():
+            form.save()
+            return redirect('search_calendar')
+
+    return render(request, 'update_calendar_complete.html', {'form': form, 'calendar': calendar})
 
 def db(request):
     if request.method == 'POST':
