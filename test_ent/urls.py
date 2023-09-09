9dec229923f310cbcef857d28e0c3c5eeb46538b
theo-ai <71651589+theo-ai@users.noreply.github.com>
2023-09-09 07:23:48 +0300

    Update urls.py (back to 2 versions before)
    but i cant lose time to change page to revert

--- test_ent/urls.py
+++ test_ent/urls.py
@@ -5,12 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('db/', views.db, name='db'),
     path('search_client/', views.search_client, name='search_client'),
-    #there is no need for that
-    #path('search_complete/', views.search_complete, name='search_complete'),
+    path('search_complete/', views.search_complete, name='search_complete'),
     path('display_client/<int:client_id>/', views.display_client, name='display_client'),
     path('insert_client/', views.insert_client, name='insert_client'),
-    #there is no need for that
-    #path('insert_complete/', views.insert_complete, name='insert_complete'),
+    path('insert_complete/', views.insert_complete, name='insert_complete'),
     path('update_client/<int:client_id>/', views.update_client, name='update_client'),
     path('update_client_complete/<int:client_id>/', views.update_client_complete, name='update_client_complete'),
     path('search_calendar/', views.search_calendar, name='search_calendar'),
