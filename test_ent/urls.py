3fa2a620346d2452b0a0b52dfaf403a81ec1576a
theo-ai <71651589+theo-ai@users.noreply.github.com>
2023-09-09 07:19:13 +0300

    Update urls.py (commented not needed line)

--- test_ent/urls.py
+++ test_ent/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     #path('search_complete/', views.search_complete, name='search_complete'),
     path('display_client/<int:client_id>/', views.display_client, name='display_client'),
     path('insert_client/', views.insert_client, name='insert_client'),
-    path('insert_complete/', views.insert_complete, name='insert_complete'),
+    #there is no need for that
+    #path('insert_complete/', views.insert_complete, name='insert_complete'),
     path('update_client/<int:client_id>/', views.update_client, name='update_client'),
     path('update_client_complete/<int:client_id>/', views.update_client_complete, name='update_client_complete'),
     path('search_calendar/', views.search_calendar, name='search_calendar'),
